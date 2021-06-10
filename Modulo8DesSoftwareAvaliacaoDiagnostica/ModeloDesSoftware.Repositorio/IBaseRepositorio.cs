f9bf725efeb5051ae942a8ac363089021f12c35a
Andre Lopes Russo <andrelrusso@gmail.com>
2021-06-09 23:32:36 -0300

    Modulo 8 Desenvolvimento de Software

---
+++ Modulo8DesSoftwareAvaliacaoDiagnostica/ModeloDesSoftware.Repositorio/IBaseRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ModeloDesSoftware.Repositorio
+{
+    public interface IBaseRepositorio
+    {
+       
+    }
+}
---
+++ Modulo8DesSoftwareAvaliacaoDiagnostica/ModuloDesSoftware/Models/ClienteViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ModuloDesSoftware.Models
+{
+    public class ClienteViewModel
+    {
+        public int IdCliente { get; set; }
+        public string NomeCliente { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Produto { get; set; }
+        public double PrecoVenda { get; set; }
+    }
+}
